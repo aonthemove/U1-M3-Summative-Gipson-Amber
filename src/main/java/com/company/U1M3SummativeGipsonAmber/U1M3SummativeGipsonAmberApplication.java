24e041676e4afffb94e205308520f62a0ddf88b4
Aonthemove <ambergipson86@gmail.com>
2019-07-18 14:16:51 -0500

    initial commit

---
+++ src/main/java/com/company/U1M3SummativeGipsonAmber/U1M3SummativeGipsonAmberApplication.java
@@ -0,0 +1,13 @@
+package com.company.U1M3SummativeGipsonAmber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M3SummativeGipsonAmberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M3SummativeGipsonAmberApplication.class, args);
+	}
+
+}
